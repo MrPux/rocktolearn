9fc022ff3144ba7b679cb8ce95a02dbde96e4e6d
Blair <blairbarnes17@gmail.com>
2025-02-08 23:49:47 -0500

    Modified survey and middle man

---
+++ auth/questions.js
@@ -0,0 +1,34 @@
+let questions = [
+    {
+        id: "q1",  // Unique ID for question 1
+        text: "What is your ideal concert experience?",
+        options: ["A. Laid-back and intimate show", "B. Spectacle guitar solos", "C. Theatrical performance", "D. Raw and unpolished"]
+    },
+    {
+        id: "q2",  // Unique ID for question 2
+        text: "What’s your favorite type of vocal performance?",
+        options: ["A. Harmonized vocals", "B. High-energy screams", "C. Dramatic, operatic-style", " D. A deep, grungy voice"]
+    },
+    {
+        id: "q3",  // Unique ID for question 3
+        text: "How do you express your creativity?",
+        options: ["A. Exploration and Experimentation", " B. Creating powerful moments", "C. Flair for the dramatic ", " D.  unfiltered emotions, "]
+    },
+    {
+        id: "q4",  // Unique ID for question 4
+        text: "What type of music do you prefer to listen to?",
+        options: ["A. Classic, timeless hits", " B. Heavy powerful sounds", " C. Energetic dynamic rock", "D. Grunge and raw "]
+    },
+    {
+        id: "q5",  // Unique ID for question 5
+        text: "What inspires your mood the most?",
+        options: [" A. Nostalgic tunes", " B .Nature and Freedom,", "C. Empowering message.", "D. Pursuit of authenticity."]
+    }
+];
+
+function displayQuestion(index){
+    const question = questions[index];
+    getElement 
+
+
+}
